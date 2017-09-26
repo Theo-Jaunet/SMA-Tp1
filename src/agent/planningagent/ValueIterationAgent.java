0ae2030763fe35e3043913cca904ea320f1a08f8
Brice <brice.denoun@gmail.com>
2017-09-26 13:32:21 +0200

    File completed and working agent

--- src/agent/planningagent/ValueIterationAgent.java
+++ src/agent/planningagent/ValueIterationAgent.java
@@ -1,25 +1,37 @@
 package agent.planningagent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.*;
+import java.util.Random;
 
-import environnement.*;
 import util.HashMapUtil;
 
+import java.util.HashMap;
+
+import environnement.Action;
+import environnement.Etat;
+import environnement.IllegalActionException;
+import environnement.MDP;
+import environnement.Action2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Cet agent met a jour sa fonction de valeur avec value iteration
- * et choisit ses actions selon la politique calculee.
+ * Cet agent met a jour sa fonction de valeur avec value iteration et choisit
+ * ses actions selon la politique calculee.
  *
  * @author laetitiamatignon
+ *
  */
 public class ValueIterationAgent extends PlanningValueAgent {
+
     /**
      * discount facteur
      */
     protected double gamma;
-
     private final double defaultV0 = 0.0;
-
     private int nbIterations;
 
     /**
@@ -28,6 +40,7 @@ public class ValueIterationAgent extends PlanningValueAgent {
     protected HashMap<Etat, Double> V;
 
     /**
+     *
      * @param gamma
      * @param nbIterations
      * @param mdp
@@ -47,29 +60,28 @@ public class ValueIterationAgent extends PlanningValueAgent {
         //*** VOTRE CODE
     }
 
-
     public ValueIterationAgent(MDP mdp) {
         this(0.9, mdp);
 
     }
 
     /**
-     * Mise a jour de V: effectue UNE iteration de value iteration (calcule V_k(s) en fonction de V_{k-1}(s'))
-     * et notifie ses observateurs.
-     * Ce n'est pas la version inplace (qui utilise nouvelle valeur de V pour mettre a jour ...)
+     *
+     * Mise a jour de V: effectue UNE iteration de value iteration (calcule
+     * V_k(s) en fonction de V_{k-1}(s')) et notifie ses observateurs. Ce n'est
+     * pas la version inplace (qui utilise nouvelle valeur de V pour mettre a
+     * jour ...)
      */
     @Override
-    public void updateV() throws Exception {
+    public void updateV() {
         //delta est utilise pour detecter la convergence de l'algorithme
         //lorsque l'on planifie jusqu'a convergence, on arrete les iterations lorsque
-        //delta < epsilon
+        //delta < epsilon 
         this.delta = 0.0;
 
-        HashMap<Etat,Double> oldV = (HashMap<Etat, Double>) V.clone();
+        HashMap<Etat, Double> oldV = (HashMap<Etat, Double>) V.clone();
 
         //*** VOTRE CODE
-
-
         for (Map.Entry<Etat, Double> entry : oldV.entrySet()) {
             Etat s = entry.getKey();
             if (!mdp.estAbsorbant(s)) {
@@ -79,7 +91,12 @@ public class ValueIterationAgent extends PlanningValueAgent {
 
                 for (Action a : actions) {
 
-                    Map<Etat, Double> transProba = mdp.getEtatTransitionProba(s, a);
+                    Map<Etat, Double> transProba = null;
+                    try {
+                        transProba = mdp.getEtatTransitionProba(s, a);
+                    } catch (Exception ex) {
+                        Logger.getLogger(ValueIterationAgent.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     double sommeListeSp = 0.0;
 
                     for (Map.Entry<Etat, Double> entry2 : transProba.entrySet()) {
@@ -89,102 +106,113 @@ public class ValueIterationAgent extends PlanningValueAgent {
 
                         sommeListeSp += prob * (recomp + this.getGamma() * oldV.get(sp));
                     }
-
                     listefinale.add(sommeListeSp);
-
                 }
-
-                V.put(s,Collections.max(listefinale));
-
+                V.put(s, Collections.max(listefinale));
+                double tempDelta = Math.abs(V.get(s) - oldV.get(s));
+                if (tempDelta > this.delta){
+                    this.delta = tempDelta;
+                }
             }
         }
 
         this.vmax = Collections.max(V.values());
         this.vmin = Collections.min(V.values());
-
+        //*** VOTRE CODE
 
         // mise a jour vmax et vmin pour affichage du gradient de couleur:
         //vmax est la valeur de max pour tout s de V
         //vmin est la valeur de min pour tout s de V
         // ...
-
-
-        //******************* laisser notification a la fin de la methode
+        //******************* laisser notification a la fin de la methode	
         this.notifyObs();
     }
 
-
     /**
-     * renvoi l'action executee par l'agent dans l'etat e
-     * Si aucune actions possibles, renvoi Action2D.NONE
+     * renvoi l'action executee par l'agent dans l'etat e Si aucune actions
+     * possibles, renvoi Action2D.NONE
      */
     @Override
     public Action getAction(Etat e) {
         //*** VOTRE CODE
-
-        return Action2D.NONE;
+        List<Action> possibleActions = new ArrayList<Action>();
+        possibleActions = this.getPolitique(e);
+        if (possibleActions.isEmpty()){
+            return Action2D.NONE;
+        }
+        else{
+            if (possibleActions.size() == 1){
+                return possibleActions.get(0);
+            }
+            else{
+                Random rand = new Random();
+                int randomNum = rand.nextInt((possibleActions.size()) + 1);
+                return possibleActions.get(randomNum);
+            }
+        }
 
     }
 
     @Override
     public double getValeur(Etat _e) {
         //*** VOTRE CODE
-
         return V.get(_e);
     }
 
     /**
-     * renvoi action(s) de plus forte(s) valeur(s) dans etat
-     * (plusieurs actions sont renvoyees si valeurs identiques, liste vide si aucune action n'est possible)
+     * renvoi action(s) de plus forte(s) valeur(s) dans etat (plusieurs actions
+     * sont renvoyees si valeurs identiques, liste vide si aucune action n'est
+     * possible)
      */
     @Override
     public List<Action> getPolitique(Etat _e) {
         //*** VOTRE CODE
-
-g
-        for (Map.Entry<Etat, Double> entry : V.entrySet()) {
-            Etat s = entry.getKey();
-            if (!mdp.estAbsorbant(s)) {
-                ArrayList<Action> actions = (ArrayList<Action>) mdp.getActionsPossibles(s);
-
-                ArrayList<Double> listefinale = new ArrayList<Double>();
-
-                for (Action a : actions) {
-                    try {
-                        Map<Etat, Double> transProba = mdp.getEtatTransitionProba(s, a);
-                    }
-                    catch (Exception e) {
-                        System.out.println(e);
-                    }
-                    double sommeListeSp = 0.0;
-
-                    for (Map.Entry<Etat, Double> entry2 : transProba.entrySet()) {
-                        Etat sp = entry2.getKey();
-                        Double prob = entry2.getValue();
-                        double recomp = mdp.getRecompense(s, a, sp);
-
-                        sommeListeSp += prob * (recomp + this.getGamma() * oldV.get(sp));
-                    }
-
-                    listefinale.add(sommeListeSp);
-
-                }
-
-            }
-        }
-
-        // retourne action de meilleure valeur dans _e selon V,
+        // retourne action de meilleure valeur dans _e selon V, 
         // retourne liste vide si aucune action legale (etat absorbant)
         List<Action> returnactions = new ArrayList<Action>();
+        if (!mdp.estAbsorbant(_e)) {
+            ArrayList<Action> actions = (ArrayList<Action>) mdp.getActionsPossibles(_e);
+            HashMap<Action, Double> mapActionValue = new HashMap<Action, Double>();
 
+            for (Action a : actions) {
+                Map<Etat, Double> transProba = null;
+                try {
+                    transProba = mdp.getEtatTransitionProba(_e, a);
+                } catch (Exception ex) {
+                    Logger.getLogger(ValueIterationAgent.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                double sommeListeSp = 0.0;
+
+                for (Map.Entry<Etat, Double> entry2 : transProba.entrySet()) {
+                    Etat sp = entry2.getKey();
+                    Double prob = entry2.getValue();
+                    double recomp = mdp.getRecompense(_e, a, sp);
+
+                    sommeListeSp += prob * (recomp + this.getGamma() * V.get(sp));
+                }
+                mapActionValue.put(a, sommeListeSp);
+            }
+            double maxv = Double.NEGATIVE_INFINITY;
+            for (Map.Entry<Action, Double> couple : mapActionValue.entrySet()) {
+                Double val = couple.getValue();
+                if (val > maxv) {
+                    maxv = val;
+                }
+            }
+            for (Map.Entry<Action, Double> couple : mapActionValue.entrySet()) {
+                Double val = couple.getValue();
+                if (val == maxv) {
+                    returnactions.add(couple.getKey());
+                }
+            }
+        }
         return returnactions;
-
     }
 
     @Override
     public void reset() {
         super.reset();
-
 
         this.V.clear();
         for (Etat etat : this.mdp.getEtatsAccessibles()) {
@@ -192,7 +220,6 @@ g
         }
         this.notifyObs();
     }
-
 
     public HashMap<Etat, Double> getV() {
         return V;
@@ -207,6 +234,5 @@ g
         System.out.println("gamma= " + gamma);
         this.gamma = _g;
     }
-
 
 }
